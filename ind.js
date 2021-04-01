a34d1e731a88fd26cebc818a7e869cf92f3bca3e
Samuel-Ng <38376202+Samuel-Ng@users.noreply.github.com>
2021-04-01 12:11:49 +0800

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('#blueStar').mouseenter(function(){
+	$(this).fadeOut('slow').fadeIn(500);});
+	
+	$('#redStar').mouseenter(function(){
+	$(this).fadeOut('slow').fadeIn(500);});
+	
+	$('#yellowStar').mouseenter(function(){
+	$(this).fadeOut('slow').fadeIn(500);});
+	
+	$('#greyStar').mouseenter(function(){
+	$(this).fadeOut('slow').fadeIn(500);});
+	
+	$('#greenStar').mouseenter(function(){
+	$(this).fadeOut('slow').fadeIn(500);});
+	
+});
